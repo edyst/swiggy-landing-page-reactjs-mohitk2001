45b2d5d94fd8da3caa819369f29b5993e58e56b8
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2021-09-18 12:42:16 +0530

    added two new component

--- src/Components/JumboSection/Jumbo.js
+++ src/Components/JumboSection/Jumbo.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import "./Jumbo.css";
 import Box from "./Box";
+import Login from "./Login";
+import Signup from "./Signup";
 function Jumbo() {
   
   const [animeText, setanimeText] = useState("Movie marathon?");
-    
-
+  const handleLogin=()=>{
+    console.log("login")
+  }
+  const handleSignUp=()=>{
+    console.log("handleSignUp")
+  }
   return (
     <div className="jumbo">
       <div className="jumboLeft">
@@ -15,8 +21,8 @@ function Jumbo() {
           <h1>SWIGGY</h1>
           </div>
           <div className="buttons">
-            <button className="login">Login</button>
-            <button className="signUp">Sign up</button>
+            <button className="login" onClick={handleLogin}>Login</button>
+            <button className="signUp" onClick={handleSignUp}>Sign up</button>
           </div>
         </div>
         <Box/>
@@ -26,8 +32,11 @@ function Jumbo() {
           src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_1340/Breakfast2-new_men3ne"
           alt="topPic"
         />
+        <Login/>
       </div>
     </div>
+   
+   
   );
 }
 
---
+++ src/Components/JumboSection/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Jumbo.css"
+function Login() {
+    return (
+        <div className="loginCompo">
+            <h1>Login</h1>
+        </div>
+    )
+}
+
+export default Login
